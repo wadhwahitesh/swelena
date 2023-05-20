0570cc8148f403b42f73fa89d3522b066408659b
Manraj Chalokia <mchalokia@umass.edu>
2023-05-20 14:59:45 -0400

    initial setup for mvc

---
+++ src/Controller/controller.py
@@ -0,0 +1,3 @@
+class Controller:
+    def calc_shortest_path(self, start_loc, end_loc, percentage, minmax_option):
+        return "hello from controller"
--- src/Controller/server.py
+++ src/Controller/server.py
@@ -1,9 +1,10 @@
 import logging
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from src import config
-
+import json
+from src.Controller import controller
 
 app = Flask(__name__)
 
@@ -13,7 +14,34 @@ def view():
     """
     This is the starting point of the application.
     """
-    return render_template("index.html", ACCESS_KEY=config.key_dict['mapview_key'])
+    return render_template("index.html", MAP_VIEW_ACCESS_KEY=config.key_dict['mapview_key'])
+
+@app.route("/calc_route", methods=["POST"])
+def calc_route():
+    """
+    This API is called when uer clicks on generate path button on UI.
+    """
+
+    user_input = request.get_json(force=True)
+
+    output = generate_map_for_user(
+        start_loc=(user_input["start_loc"]["lat"], user_input["start_loc"]["lng"]),
+        end_loc=(user_input["end_loc"]["lat"], user_input["end_loc"]["lng"]),
+        percentage=user_input["percentage"],
+        minmax_option=user_input["minmax_option"]
+    )
+
+    return json.dumps(output)
+
+def generate_map_for_user(start_loc, end_loc, percentage, minmax_option):
+    """
+    The main function which calculates the path given the user constraints.
+    """
+
+    controller = controller.Controller()
+    path = controller.calc_shortest_path(start_loc, end_loc, percentage, minmax_option)
+    return path
+
 
 if __name__ == '__main__':
     app.run()
