251ab6beca8a0758fb0bd85277362bed62409c6f
Manraj Chalokia <mchalokia@umass.edu>
2023-05-20 14:26:36 -0400

    add API config and link to index view

--- src/Controller/server.py
+++ src/Controller/server.py
@@ -1,13 +1,19 @@
 import logging
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
-from flask import Flask
+from flask import Flask, render_template
+from src import config
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+
+@app.route("/view")
+def view():
+    """
+    This is the starting point of the application.
+    """
+    return render_template("index.html", ACCESS_KEY=config.key_dict['mapview_key'])
 
 if __name__ == '__main__':
     app.run()
